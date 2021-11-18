6d45c9ce8e5873ed590e54a05a8d6771bef2ddbc
Jonatas Afonso <jonatas_afonso@hotmail.com>
2021-11-18 21:47:39 +0000

    CRUD explicacao

--- Amazonia.ConsoleAPP/Program.cs
+++ Amazonia.ConsoleAPP/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Configuration;
+using System.Linq;
 using Amazonia.DAL.Modelo;
 using Amazonia.DAL.Repositorios;
 using Amazonia.DAL.Utils;
+using Microsoft.EntityFrameworkCore;
 
 namespace Amazonia.ConsoleAPP
 {
@@ -11,23 +13,49 @@ namespace Amazonia.ConsoleAPP
         static void Main(string[] args)
         {
             var ctx = new AmazoniaContexto();
-            ctx.Clientes.Add(new Cliente { 
-                UserName = "joao.silva",
-                DataNascimento = new DateTime(1984, 05, 29),
-                Nome = "João da Silva",
-                NumeroIdentificacaoFiscal = "123456789",
-                Password = "senha"
-            });
+
+            //CREATE
+            AdicionarClienteExemplo(ctx);
+            AdicionarLivrosExemplo(ctx);
+            //ImprimirValorBaseadoEmConfiguracao();
+
+            //READ
+            var livroEscolhido = ctx.Livros.FirstOrDefault(x => x.Nome.StartsWith("Harry"));
+            Console.WriteLine(livroEscolhido);
+
+
+            var primeiroAudioLivroEncontrato = ctx.AudioLivros.FirstOrDefault(x => x.DuracaoLivroEmMinutos >= 10);
+            Console.WriteLine(primeiroAudioLivroEncontrato);
+
+
+            //UPDATE
+            primeiroAudioLivroEncontrato.DuracaoLivroEmMinutos = 90;
             ctx.SaveChanges();
 
 
 
+            //Delete
+            //var primeiroLivroImpressto = ctx.LivroImpressos.FirstOrDefault();
+            //Console.WriteLine(primeiroLivroImpressto);
+            //ctx.LivroImpressos.Remove(primeiroLivroImpressto);
+            //ctx.SaveChanges();
 
 
+            var exemploLeituraComSql = ctx.Livros.FromSqlRaw("SELECT TOP 10 * FROM LIVROS");
+
+            foreach (var item in exemploLeituraComSql)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        private static void ImprimirValorBaseadoEmConfiguracao()
+        {
             var valorObtidoPeloMetodo = Exemplo.ObterValorDoConfig("chaveExemplo");
 
 
-            var chaveExemplo = ConfigurationManager.AppSettings["chaveExemplo"];            
+            var chaveExemplo = ConfigurationManager.AppSettings["chaveExemplo"];
 
             var usarRegranovaStr = ConfigurationManager.AppSettings["regraNovaAtiva"];
             var usarRegranova = Convert.ToBoolean(usarRegranovaStr);
@@ -39,11 +67,66 @@ namespace Amazonia.ConsoleAPP
             else
             {
                 ListarLivros();
-            }           
-
-
+            }
         }
-    
+
+        private static void AdicionarLivrosExemplo(AmazoniaContexto ctx)
+        {
+            var livroDigigal = new LivroDigital
+            {
+                Nome = "Harry Potter Digital",
+                Autor = "JK",
+                Descricao = "Livro harry potter",
+                FormatoFicheiro = "PDF",
+                Idioma = DAL.Idioma.Portugues,
+                InformacoesLicenca = "",
+                Preco = 100,
+                TamanhoEmMB = 10
+            };
+
+            var audioLivro = new AudioLivro
+            {
+                Nome = "Harry Potter Audio Livro",
+                Autor = "JK",
+                Descricao = "Livro harry potter",
+                FormatoFicheiro = "MP3",
+                Idioma = DAL.Idioma.Portugues,
+                Preco = 100,
+                DuracaoLivroEmMinutos = 60
+            };
+
+
+            var livroImpresso = new LivroImpresso
+            {
+                Nome = "Harry Potter Impresso",
+                Autor = "JK",
+                Descricao = "Livro harry potter",
+                Idioma = DAL.Idioma.Portugues,
+                Preco = 100,
+                Altura = 10,
+                Largura = 10,
+                Profundidade = 10
+            };
+
+            ctx.Livros.Add(livroImpresso);
+            ctx.Livros.Add(audioLivro);
+            ctx.Livros.Add(livroDigigal);
+            ctx.SaveChanges();
+        }
+
+        private static void AdicionarClienteExemplo(AmazoniaContexto ctx)
+        {
+            ctx.Clientes.Add(new Cliente
+            {
+                UserName = "joao.silva",
+                DataNascimento = new DateTime(1984, 05, 29),
+                Nome = "João da Silva",
+                NumeroIdentificacaoFiscal = "123456789",
+                Password = "senha"
+            });
+            ctx.SaveChanges();
+        }
+
         public static void ListarLivros(){
             
             var repo = new RepositorioLivro();
