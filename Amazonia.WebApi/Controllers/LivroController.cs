9e936bff34c1261190754f45b8a0e2eaec2d59bc
Jonatas Afonso <jonatas_afonso@hotmail.com>
2021-12-16 20:10:53 +0000

    Adicao de WebApi

---
+++ Amazonia.WebApi/Controllers/LivroController.cs
@@ -0,0 +1,22 @@
+﻿using Amazonia.DAL.Modelo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amazonia.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        [HttpGet]
+        public List<Livro> GetLivros()
+        {
+            var ctx = new AmazoniaContexto();
+            return ctx.Livros.ToList();
+        } 
+    }
+}
