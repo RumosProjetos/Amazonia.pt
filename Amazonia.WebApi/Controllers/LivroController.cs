c30c5e57cdc8c87fe3b616c7cdf629ca72562106
Jonatas Afonso <jonatas_afonso@hotmail.com>
2021-12-16 20:31:06 +0000

    Exemplo Api Livros

--- Amazonia.WebApi/Controllers/LivroController.cs
+++ Amazonia.WebApi/Controllers/LivroController.cs
@@ -1,4 +1,5 @@
 ﻿using Amazonia.DAL.Modelo;
+using Amazonia.WebApi.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,11 +13,49 @@ namespace Amazonia.WebApi.Controllers
     [ApiController]
     public class LivroController : ControllerBase
     {
+        AmazoniaContexto ctx;
+        public LivroController()
+        {
+            ctx = new AmazoniaContexto();
+        }
+
         [HttpGet]
         public List<Livro> GetLivros()
         {
-            var ctx = new AmazoniaContexto();
             return ctx.Livros.ToList();
-        } 
+        }
+
+        [HttpGet("{id}")]
+        public Livro GetLivro(Guid id)
+        {
+            return ctx.Livros.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public Guid PostLivroNovo(LivroDto livro)
+        {
+            Livro livroNovo = new LivroImpresso();
+            switch (livro.TipoLivro)
+            {
+                case EnumTipoLivro.LivroImpresso:
+                    livroNovo = new LivroImpresso();
+                    break;
+                case EnumTipoLivro.LivroDigital:
+                    livroNovo = new LivroDigital();
+                    break;
+                case EnumTipoLivro.AudioLivro:
+                    livroNovo = new AudioLivro();
+                    break;
+            }
+
+            livroNovo.Nome = livro.Nome;
+            livroNovo.Autor = livro.Autor;
+            livroNovo.Descricao = livro.Descricao;
+
+            ctx.Livros.Add(livroNovo);
+            return livroNovo.Id;
+        }
     }
+
+
 }
---
+++ Amazonia.WebApi/Dto/LivroDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Amazonia.WebApi.Dto
+{
+    public class LivroDto
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Autor { get; set; }
+        public EnumTipoLivro TipoLivro { get; set; } //TODO: Resolver depois
+    }
+}
