457a3fe123d63b54e4c3aee3e5a757dab568c0cf
Jonatas Afonso <jonatas_afonso@hotmail.com>
2022-02-10 21:49:41 +0000

    Adicionado profile Sonar Lint Validacao de Codigo

--- VNET60/Amazonia.Web.WebForms/About.aspx.cs
+++ VNET60/Amazonia.Web.WebForms/About.aspx.cs
@@ -10,9 +10,35 @@ namespace Amazonia.Web.WebForms
 
         }
 
-        protected void Button1_Click(object sender, EventArgs e)
+        public void exemplo()
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var exemplo = "";
+
+            if (true)
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    for (int j = 0; j < 1000; i++)
+                    {
+                        for (int h = 0; h < 1000; i++)
+                        {
+                            for (int l = 0; l < 1000; i++)
+                            {
+                                switch (i)
+                                {
+                                    default:
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- VNET60/Amazonia.eCommerceRazor/Program.cs
+++ VNET60/Amazonia.eCommerceRazor/Program.cs
@@ -8,6 +8,22 @@ namespace Amazonia.eCommerceRazor
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+
+            var teste = "";
+
+
+
+            if (true)
+            {
+
+            }
+        }
+
+
+        public string exemplo(string a, string b, string b2, string b3, string b4, string b5, string b6, string b7, string b8, string b9, string b10, string b11)
+        {
+            return a + b + b3 + b2 + b4 + b6 + b5;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -16,6 +32,9 @@ namespace Amazonia.eCommerceRazor
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        
+
     }
 }
 
